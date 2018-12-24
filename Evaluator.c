5c47de3e802a5f67d7de5c51ef5d807d1537d67f
maoruimas <maoruimas@163.com>
2018-12-24 10:42:30 +0800

    Update Evaluator.c
    Rewrite evaLine() to make sure that evaPoint() and evaBoard() work well.

--- Evaluator.c
+++ Evaluator.c
@@ -43,49 +43,61 @@ void revRole(char s[])
 {
 	for(;*s;++s)*s=(*s=='2'?'2':(*s=='1'?'0':'1'));
 }
-int evaLine(char s[],int role)
-{
-	int ret=0;
-	if(role=='0')revRole(s);
-	ret+=acSearch(s);
-	revRole(s);
-	ret-=acSearch(s);
-	return ret;
-}
-int evaPoint(char board[][BOARD_SIZE],int x,int y,int role)
+int evaLine(char board[][BOARD_SIZE],int x,int y,int type,int role)
 {
 	int i,j,ret;
 	char tp[BOARD_SIZE];
 	ret=0;
-	for(j=0;j<BOARD_SIZE;++j)
-        tp[j]=board[x][j];
-    tp[BOARD_SIZE]=0;
-    ret+=evaLine(tp,role);
-    for(i=0;i<BOARD_SIZE;++i)
-        tp[i]=board[i][y];
-    tp[BOARD_SIZE]=0;
-    ret+=evaLine(tp,role);
-	if((i=x+y)&&i>=4&&i<2*BOARD_SIZE-5)
-    {
-		for(j=0;j<BOARD_SIZE-abs(i-BOARD_SIZE+1);++j)
-			tp[j]=board[min(i,BOARD_SIZE-1)-j][max(0,i-BOARD_SIZE+1)+j];
-		tp[BOARD_SIZE-abs(i-BOARD_SIZE+1)]=0;
-		ret+=evaLine(tp,role);
-    }
-    if((i=x-y+BOARD_SIZE-1)&&i>=4&&i<2*BOARD_SIZE-5)
-    {
-		for(j=0;j<BOARD_SIZE-abs(i-BOARD_SIZE+1);++j)
-			tp[j]=board[min(i,BOARD_SIZE-1)-j][min(BOARD_SIZE-1,2*BOARD_SIZE-2-i)-j];
-		tp[BOARD_SIZE-abs(i-BOARD_SIZE+1)]=0;
-        ret+=evaLine(tp,role);
+	switch(type)
+	{
+		case 1:
+			for(j=0;j<BOARD_SIZE;++j)
+        		tp[j]=board[x][j];
+    		tp[BOARD_SIZE]=0;
+			break;
+		case 2:
+    		for(i=0;i<BOARD_SIZE;++i)
+        		tp[i]=board[i][y];
+    		tp[BOARD_SIZE]=0;
+			break;
+		case 3:
+			i=x+y;
+			for(j=0;j<BOARD_SIZE-abs(i-BOARD_SIZE+1);++j)
+				tp[j]=board[min(i,BOARD_SIZE-1)-j][max(0,i-BOARD_SIZE+1)+j];
+			tp[BOARD_SIZE-abs(i-BOARD_SIZE+1)]=0;
+			break;
+		case 4:
+			i=x-y+BOARD_SIZE-1;
+			for(j=0;j<BOARD_SIZE-abs(i-BOARD_SIZE+1);++j)
+				tp[j]=board[min(i,BOARD_SIZE-1)-j][min(BOARD_SIZE-1,2*BOARD_SIZE-2-i)-j];
+			tp[BOARD_SIZE-abs(i-BOARD_SIZE+1)]=0;
+			break;
+		default:
+			break;
 	}
+	if(role==WHITE)revRole(tp);
+	ret+=acSearch(tp);
+	revRole(tp);
+	ret-=acSearch(tp);
 	return ret;
 }
-int s15[15]={0,2,4,1,9,11,13,3,12,8,5,14,6,10,7};
+int evaPoint(char board[][BOARD_SIZE],int x,int y,int role)
+{
+	int i,ret=0;
+	for(i=1;i<=4;++i)
+		ret+=evaLine(board,x,y,i,role);
+	return ret;
+}
 int evaBoard(char board[][BOARD_SIZE],int role)
 {
-    int i,ret;
-    ret=0;
-    for(i=0;i<BOARD_SIZE;++i){ret+=evaPoint(board,i,s15[i],role);printf("ret=%d\n",ret);}
-    return ret;
+    int i,j,ret=0;
+	for(i=0;i<BOARD_SIZE;++i)
+	{
+		for(j=2;j<5;++j)
+			ret+=evaLine(board,0,i,j,role);
+		ret+=evaLine(board,i,0,1,role);
+		ret+=evaLine(board,i,0,4,role);
+		ret+=evaLine(board,i,BOARD_SIZE-1,3,role);
+	}
+	return ret;
 }
